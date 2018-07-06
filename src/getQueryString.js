b3a8280c5b8bfd26ebdbda19c67ec9d98fe34080
changzhn <changzhn@yonyou.com>
2018-07-06 15:40:56 +0800

    eslint-fix

--- src/getQueryString.js
+++ src/getQueryString.js
@@ -2,7 +2,7 @@
 	 * getQueryString：获取query中某个name对应的值
 	 */
 export default function() {
-	var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)');
-	var r = window.location.search.substr(1).match(reg);
-	return r ? decodeURIComponent(r[2]) : '';
+  var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)');
+  var r = window.location.search.substr(1).match(reg);
+  return r ? decodeURIComponent(r[2]) : '';
 }
--- src/uuid.js
+++ src/uuid.js
@@ -2,23 +2,23 @@
 	* uuid：随机生成一个 16进制的id 4eef3609-5b83-4408-866a-aee202bf1a20
 	*/
 export default function () {
-	var i,
-		random;
-	var uuid = '';
+  var i,
+    random;
+  var uuid = '';
 
-	for (i = 0; i < 32; i++) {
-		random = Math.random() * 16 | 0;
-		if (i === 8 || i === 12 || i === 16 || i === 20) {
-			uuid += '-';
-		}
-		uuid += (
-			i === 12
-				? 4
-				: (
-					i === 16
-						? (random & 3 | 8)
-						: random)).toString(16);
-	}
+  for (i = 0; i < 32; i++) {
+    random = Math.random() * 16 | 0;
+    if (i === 8 || i === 12 || i === 16 || i === 20) {
+      uuid += '-';
+    }
+    uuid += (
+      i === 12
+        ? 4
+        : (
+          i === 16
+            ? (random & 3 | 8)
+            : random)).toString(16);
+  }
 
-	return uuid;
+  return uuid;
 }
