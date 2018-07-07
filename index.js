574584c67db0f2aa31f910d74d2860b3564e7f0c
changzhn <changzhn@yonyou.com>
2018-07-07 10:04:55 +0800

    v2.0.3

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 let uuid = require('./src/uuid');
 let formatTime = require('./src/formatTime');
-let query = require('./src/query');
 let getQueryString = require('./src/getQueryString');
 
 module.exports = {
   uuid,
   formatTime,
-  query,
   getQueryString
 };
